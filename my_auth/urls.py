921313648063b60cbe359b4b8d4610237ce6c5f0
juntana <junichi.tanaka@persol.co.jp>
2017-09-24 09:13:10 +0000

    フォームの変更で認証が出来なくなっていたので、修正

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-
-import views
+from forms import MyAuthForm
 
 app_name = 'my_auth'
 urlpatterns = [
-    url(r'^in/$',views.login, name='login'),
+    url(r'^in/$', auth_views.login, {'template_name': 'my_auth/login.html','authentication_form': MyAuthForm}, name='login'),
     url(r'^out/$',auth_views.logout,{'template_name': 'my_auth/logout.html'},name='logout'),
 ]
