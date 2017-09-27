f5a960d87be5f58ad2dd8422ac03d92fbe4749d0
ryo <nakamura4045@gmail.com>
2017-09-27 05:28:03 +0000

    ユーザー修正ページとりあえず動く

--- persol_users/urls.py
+++ persol_users/urls.py
@@ -8,7 +8,7 @@ app_name = 'persol_users'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<user_id>[0-9]+)/$', views.detail, name='detail'),
-#    url(r'^user_modify/$', views.user_modify, name='user_modify'),
+    url(r'^user_modify/$', views.user_modify, name='user_modify'),
     url(r'^user_add/$', views.user_add, name='user_add'),
     url(r'^user_add_operation/$', views.user_add_operation, name='user_add_operation'),
 ]
