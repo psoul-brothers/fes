27a30ea6fc774c30b18b83ea97601fcc3a432943
shunsuket <shunsuke.t3@gmail.com>
2017-09-28 14:02:17 +0000

    整理した。機能は追加していない。

--- events/views.py
+++ events/views.py
@@ -17,14 +17,12 @@ def event_index(request):
         search_word = request.POST['word'] # 検索の値が空白でも大丈夫
         search_results = Event.objects.filter(
                 Q(event_name__contains = search_word) | 
-                Q(overview__contains = search_word)
+                Q(overview__contains   = search_word)
             )
         event_list = search_results
         if request.POST['sort'] == 'like':
-            print("now!!!!!")
             event_list = event_list.annotate(like_num = Count('like')).order_by('-like_num')
-        if request.POST['sort'] == 'watch':
-            print("now!!!!!!!!!!!!!!!")
+        elif request.POST['sort'] == 'watch':
             event_list = event_list.annotate(watch_num = Count('watch')).order_by('-watch_num')
         """降順指定したい場合
         if request.POST['sort'] == "desc":
@@ -32,14 +30,15 @@ def event_index(request):
         """
     else:
         event_list = Event.objects.order_by('id')
+    # get each event
+    latest_events    = event_list
+    joing_events     = event_list.filter(Q(members = request.user.id))
+    watching_events  = event_list.filter(Q(watch   = request.user.id))
+    organized_events = event_list.filter(Q(author  = request.user.id))
+    
     member_list = PersolUser.objects.order_by('id')
     form = SelectUserForm()
     like_form = LikeUserForm()
-    # get each event
-    latest_events   = event_list
-    joing_events    = event_list.filter(Q(members = request.user.id))
-    watching_events = event_list.filter(Q(watch   = request.user.id))
-    organized_events= event_list.filter(Q(author  = request.user.id))
     context = {
         'member_list'      : member_list,
         'form'             : form,
@@ -52,7 +51,6 @@ def event_index(request):
     return render(request, 'events/index.html', context)
     
 def event_create(request):
-    #Event = get_object_or_404(Event, Event.id)
     if request.method == 'POST':
         form = CreateForm(request.POST)
         if form.is_valid(): # バリデーションを通った
@@ -60,30 +58,27 @@ def event_create(request):
             # ...
             login_user = get_object_or_404(PersolUser, id=request.user.id)
             e = Event(
-                author=login_user,
-                event_name=request.POST['event_name'], 
-                event_image=request.POST['event_image'], 
-                event_datetime=request.POST['event_datetime'], 
-                event_location=request.POST['event_location'], 
-                num_of_members=request.POST['num_of_members'], 
-                dead_line=request.POST['dead_line'],
-                overview=request.POST['overview']
+                author         = login_user,
+                event_name     = request.POST['event_name'], 
+                event_image    = request.POST['event_image'], 
+                event_datetime = request.POST['event_datetime'], 
+                event_location = request.POST['event_location'], 
+                num_of_members = request.POST['num_of_members'], 
+                dead_line      = request.POST['dead_line'],
+                overview       = request.POST['overview']
             )
             e.save()
-#per                e.author.add(login_user)
             return HttpResponseRedirect('/events/') # POST 後のリダイレクト
     else:
         form = CreateForm() # 非束縛フォーム
-
     return render(request, 'events/create.html', {'form': form,})
 
 
 def event_detail(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
+    event        = get_object_or_404(Event, pk=event_id)
     members_list = event.members.all()
-    like_list = event.like.all()
-    num_of_like = event.like.count()
-    print(num_of_like)
+    like_list    = event.like.all()
+    num_of_like  = event.like.count()
     watcher_list = event.watch.all()
     context = {
         'event'       : event,
@@ -92,7 +87,6 @@ def event_detail(request, event_id):
         'watcher_list': watcher_list,
         'num_of_like' : num_of_like
     }
-    context.update({'contexts':context})
     return render(request, 'events/detail.html', context)
 
 
@@ -106,9 +100,11 @@ def event_edit(request, event_id):
     else:
         form = EventForm(instance=event) # 非束縛フォーム
 
-    edit_context = {'form': form, 'event': event}
+    edit_context = {
+        'form'  : form,
+        'event' : event
+    }
     return render(request, 'events/edit.html', context=edit_context)
-
 
 def event_join(request, event_id):
     target_event = get_object_or_404(Event, id=event_id)
@@ -116,7 +112,6 @@ def event_join(request, event_id):
     target_event.members.add(new_member)
     return HttpResponseRedirect('/events/')
     
-
 def event_like(request, event_id):
     if request.POST['like'] == 'leave':
         target_event = get_object_or_404(Event, id=event_id)
@@ -131,7 +126,7 @@ def event_like(request, event_id):
         target_event = get_object_or_404(Event, id=event_id)
         new_like = get_object_or_404(PersolUser, employee_number=request.user.id)
         target_event.like.add(new_like)
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    return HttpResponseRedirect(request.META['HTTP_REFERER']) # リクエスト先にリダイレクト
 
 def event_watch(request, event_id):
     if request.POST['watch'] == 'leave':
@@ -171,7 +166,6 @@ def event_search(request):
     if request.method == 'POST':
         form = EventsSearchForm(request.POST)
         if form.is_valid():
-#            tpl      = loader.get_template('events/index.html')
             word     = form.cleaned_data['word']
             search_results = Event.objects.filter(
                 Q(event_name__contains = word) | 
@@ -185,13 +179,4 @@ def event_search(request):
             return render(request, 'events/index.html', context)
     else:
         form = EventsSearchForm()
-#    tpl = loader.get_template('events/index.html')
     return render(request, 'events/index.html', {'form' : form})
-
-"""    return HttpResponse(tpl.render(RequestContext(
-        request,
-        {
-            'form' : form
-        }
-    )))
-"""
