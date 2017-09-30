c32461e074ea6a7204c8702e1c5ea3e4f9fc7c3c
松山浩之 <h.matsuyama7@gmail.com>
2017-09-30 14:22:11 +0000

    アンケート回答の表示を作成中

--- questions/models.py
+++ questions/models.py
@@ -6,13 +6,39 @@ from persol_users.models import PersolUser
 class Question(models.Model):
     questionnaire_title = models.CharField(max_length=200)
     question_text = models.TextField(max_length=500)
+    
+    def get_users(self):
+        persol_user_ids = Answer.objects.values("persol_user").filter(question = self).distinct()
+        return PersolUser.objects.filter(id__in = persol_user_ids)
 
+    def get_answer(self,u_id,c_id):
+        answerCount = Answer.objects.filter(question = self,persol_user = u_id,choice = c_id).count()
+        if answerCount == 0:
+            return "-"
+        return Answer.objects.filter(question = self,persol_user = u_id,choice = c_id)[0].answer_text
+        
+    def get_user_answers(self):
+        user_answers_dict = []
+        choice_answer_dict = []
+        for persol_user in self.get_users():
+            for choice in self.choice_set.all:
+                if (self.get_answer(persol_user,choice) is None):
+                    continue
+                choice_answer_dict[choice.choice_text] = self.get_answer(persol_user,choice) 
+            user_answers_dict[persol_user] = choice_answer_dict
+        return user_answers_dict
+
+                
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+
 
 class Answer(models.Model):
     persol_user = models.ForeignKey(PersolUser, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
+    
+    class Meta:
+        unique_together=(("persol_user","question","choice"))
--- questions/views.py
+++ questions/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_list_or_404,get_object_or_404, render
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import Question,Choice,Answer
+from persol_users.models import PersolUser
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 import traceback
@@ -16,7 +17,7 @@ def registration(request):
         q.save()
         choiceText = request.POST['choice_text']
         for choice in choiceText.splitlines():
-            c = q.choice_set.create(choice_text=choice)
+           q.choice_set.create(choice_text=choice)
             
     except :
         response = "DBエラーにより保存できませんでした。"
@@ -27,7 +28,13 @@ def registration(request):
         
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'questions/detail.html', {'question': question})
+    choices = []
+    if (question.get_user_answers):
+        choice_answer_dict = sorted(question.user_answers.values())[0]
+        if (choice_answer_dict):
+            choices = sorted(choice_answer_dict.values())[0].keys()
+    
+    return render(request, 'questions/detail.html', {'question': question, 'choices': choices})
 
 def index(request):
     latest_question_list = Question.objects.order_by('id')
@@ -44,9 +51,15 @@ def answerRegistration(request, question_id):
     # choices = question.choice_set.all
     # choices = get_list_or_404(Choice, pk__in=request.POST.getlist('choiceId'))
     print question
+    user_id = get_object_or_404(PersolUser, pk=1)
     try:
-        for choice in question.choice_set.all() :
-            choice.answer_set.create(question_id=question_id,answer_text=request.POST['answer_text_%d' %choice.id])
+        for choice in question.choice_set.all()  :
+            print choice.id
+            answerCount = Answer.objects.filter(persol_user=user_id,question_id=question_id,choice_id=choice.id).count()
+            if answerCount > 0:
+                print answerCount
+                Answer.objects.filter(persol_user=user_id,question_id=question_id,choice_id=choice.id).delete()
+            choice.answer_set.create(persol_user=user_id,question_id=question_id, answer_text=request.POST['answer_text_%d' %choice.id])
         # question.answer_set.create(answer_text=request.POST['answer_text'])
     except :
         response = "DBエラーにより保存できませんでした。"
