842f3919cea7c95eb0e2286a0b8e6e8a70f40a60
pmaken <ctv2500@yahoo.co.jp>
2017-10-08 05:33:51 +0000

    js,CSS作成(代理コミット by 田中)

---
+++ static/js/common.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+/* メニューのサブメニュー
+http://techmemo.biz/javascript/acordionmenu/ */
+$(".submenu").css("display","none");
+$(".trigger").click(function(){
+if($("+.submenu",this).css("display")=="none"){
+$(this).addClass("active-submenu");
+$(this).removeClass("none-submenu");
+$("+.submenu",this).slideDown("normal");
+}else{
+$(this).removeClass("active-submenu");
+$(this).addClass("none-submenu");
+$("+.submenu",this).slideUp("normal");
+}
+});
+
+/* ヘッダーがスクロールで出たり消えたり
+http://weboook.blog22.fc2.com/blog-entry-412.html */
+var menuHeight = $("#menu-wrap").height();
+var startPos = 0;
+$("header").css("padding-bottom", + menuHeight + "px")
+$(window).scroll(function(){
+  var currentPos = $(this).scrollTop();
+  if (currentPos > startPos) {
+    if($(window).scrollTop() >= 200) {
+      $("#menu-wrap").css("top", "-" + menuHeight + "px");
+    }
+  } else {
+    $("#menu-wrap").css("top", 0 + "px");
+  }
+  startPos = currentPos;
+});
+
+
+});
