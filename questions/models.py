352231f04e483068ab15350debe3c31face5aa60
松山浩之 <h.matsuyama7@gmail.com>
2017-10-12 11:27:34 +0000

    アンケート回答の順序をOrderedDictを使用しソート

--- questions/models.py
+++ questions/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from persol_users.models import PersolUser
+from collections import OrderedDict
 
 class Question(models.Model):
     questionnaire_title = models.CharField(max_length=200)
@@ -12,7 +13,10 @@ class Question(models.Model):
         return PersolUser.objects.filter(id__in = persol_user_ids)
 
     def get_sorted_choices(self):
-        return self.choice_set.all().order_by('id')
+        choices = []
+        for choice in self.choice_set.all().order_by('id'):
+            choices.append(choice.choice_text)
+        return choices
 
     def get_answer(self,u_id,c_id):
         answerCount = Answer.objects.filter(question = self,persol_user = u_id,choice = c_id).count()
@@ -23,16 +27,14 @@ class Question(models.Model):
     def get_user_answers(self):
         user_answers_dict = {}
         choice_answer_dict = {}
+        choices = self.choice_set.all()
         for persol_user in self.get_users():
-            choices = self.get_sorted_choices()
             print "models"
             print choices
-            # choices = sorted(choices,key=id)
-            for choice in choices:
+            for i,choice in enumerate(choices):
                 print choice.choice_text
-                choice_answer_dict[choice.choice_text] = self.get_answer(persol_user,choice) 
-            # choice_answer_dict = sorted(choice_answer_dict.items(), key=lambda x: x[0])
-            user_answers_dict[persol_user] = choice_answer_dict
+                choice_answer_dict[i] = self.get_answer(persol_user,choice) 
+            user_answers_dict[persol_user] = OrderedDict(sorted(choice_answer_dict.items(), key=lambda x: x[0]))
         return user_answers_dict
 
                 
