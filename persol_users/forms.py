aadb76a6085fa12d852acc9069bda989def06495
ryo <nakamura4045@gmail.com>
2017-10-13 08:38:14 +0000

    ID・メールアドレスの重複確認処理実空白だとエラーになったので修正

--- persol_users/forms.py
+++ persol_users/forms.py
@@ -9,48 +9,32 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
 
-#class user_add_Form(forms.Form):
-#    employee_number = forms.CharField(label='社員番号')
-#    password = forms.CharField(label='パスワード',widget=PasswordInput(attrs={
-#         'type':"text",
-#         'onfocus':"if (this.value == 'Password') this.value = '';",
-#         'onfocus':"type='Password'",
-#    }))
-#    surname = forms.CharField(label='姓')
-#    name = forms.CharField(label='名')
-#    mail_address = forms.EmailField(label='メールアドレス')
-#    self_introduction_text = forms.CharField(label='自己紹介メッセージ',widget=forms.Textarea,required=False)
-#    data = forms.FileField(label='画像',required=False)
-    
-    
 class user_add_Form(forms.Form):
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('PersolUser','')
-        super(user_add_Form, self).__init__(*args, **kwargs)
-        
-        self.fields['employee_number'] = forms.CharField(label='社員番号')
-        self.fields['password'] = forms.CharField(label='パスワード',widget=PasswordInput(attrs={
-             'type':"text",
-             'onfocus':"if (this.value == 'Password') this.value = '';",
-             'onfocus':"type='Password'",
-        }))
-        self.fields['surname'] = forms.CharField(label='姓')
-        self.fields['name'] = forms.CharField(label='名')
-        self.fields['mail_address'] = forms.EmailField(label='メールアドレス')
-        self.fields['self_introduction_text'] = forms.CharField(label='自己紹介メッセージ',widget=forms.Textarea,required=False)
-        self.fields['data'] = forms.FileField(label='画像',required=False)
-        
-        
+
+    employee_number = forms.CharField(label='社員番号')
+    password = forms.CharField(label='パスワード',widget=PasswordInput(attrs={
+         'type':"text",
+         'onfocus':"if (this.value == 'Password') this.value = '';",
+         'onfocus':"type='Password'",
+    }))
+    surname = forms.CharField(label='姓')
+    name = forms.CharField(label='名')
+    mail_address = forms.EmailField(label='メールアドレス')
+    self_introduction_text = forms.CharField(label='自己紹介メッセージ',widget=forms.Textarea,required=False)
+    data = forms.FileField(label='画像',required=False)
+    
     def clean(self):
         cleaned_data = self.cleaned_data
         employee_number = cleaned_data.get('employee_number')
         mail_address = cleaned_data.get('mail_address')
+        
+        if employee_number is not None:
+            if PersolUser.objects.filter(employee_number__contains = employee_number).exists():
+                self._errors['employee_number'] = self.error_class(['ユーザーIDがすでに存在します'])  
 
-        if PersolUser.objects.filter(employee_number__contains = employee_number).exists():
-            self._errors['employee_number'] = self.error_class(['ユーザーIDがすでに存在します'])  
-
-        if PersolUser.objects.filter(mail_address__contains = mail_address).exists():
-            self._errors['mail_address'] = self.error_class(['メールアドレスがすでに存在します']) 
+        if mail_address is not None:
+            if PersolUser.objects.filter(mail_address__contains = mail_address).exists():
+                self._errors['mail_address'] = self.error_class(['メールアドレスがすでに存在します']) 
             
         return cleaned_data
 
