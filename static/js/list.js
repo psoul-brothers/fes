9849bf8e47d72503a37a8865bce39c9a15005264
satouhiroki <hiroki1456@gmail.com>
2017-10-15 19:42:47 +0900

    [fix]スライダーの斜め移動に閾値を追加

--- static/js/list.js
+++ static/js/list.js
@@ -1,51 +1,76 @@
 $(document).ready(function(){
 
     /******************************************
-    事前準備
-    *******************************************/
+     事前準備
+     *******************************************/
 
-    //タブボタンの数を取得
+        //タブボタンの数を取得
     var tabQuantity = $('.tab__button').length;
 
     //タブの長さとボディの長さの差分を取得
     var tabExtraDistance = $('.tab').width() - $('.tabContainer').width();
 
+    var isFlick = false;
 
-    /******************************************
-    スクロール中かどうかの判定
-    *******************************************/
-    var isScrolling = 0 ;
-    var timeoutId ;
-    window.addEventListener( "scroll", function () {
-        isScrolling = 1 ;
+    var THRESHOLD_PX = 100;
 
-        // スクロールを停止して500ms後に終了とする
-        clearTimeout( timeoutId ) ;
-
-        timeoutId = setTimeout( function () {
-            isScrolling = 0 ;
-        }, 500 ) ;
-    } ) ;
 
 
     /******************************************
-    スライダー発動
-    *******************************************/
-    if(isScrolling == 0){
+     スクロール中かどうかの判定
+     *******************************************/
+    $(window).on('touchstart', onTouchStart); //指が触れたか検知
+    $(window).on('touchmove', onTouchMove); //指が動いたか検知
+    $(window).on('touchend', onTouchEnd); //指が離れたか検知
+    var direction, position;
+
+    //スワイプ開始時の横方向の座標を格納
+    function onTouchStart(event) {
+        position = getPosition(event);
+        direction = ''; //一度リセットする
+    }
+
+    //スワイプの方向（left／right）を取得
+    function onTouchMove(event) {
+        moveRangeX = position - event.originalEvent.touches[0].pageX;
+        moveRangeY = position - event.originalEvent.touches[0].pageY;
+        if (moveRangeX > THRESHOLD_PX || (-1 * THRESHOLD_PX) > moveRangeX) {
+            $(window).on('touchmove.noScroll', function(e) {
+                e.preventDefault();
+            });
+            isFlick = true;
+        } else if (moveRangeY > THRESHOLD_PX || (-1 * THRESHOLD_PX) > moveRangeY) {
+            $(window).on('touchmove.noScroll', function(e) {
+                e.preventDefault();
+            });
+            isFlick = true;
+        }
+    }
+
+    function onTouchEnd(event) {
+        if (isFlick){
+            $(window).off('.noScroll');
+            isFlick = false;
+        }
+    }
+
+
+    /******************************************
+     スライダー発動
+     *******************************************/
     var slider = $('.contents').bxSlider({
         pager:false,
         controls:false,
+        swipeThreshold: THRESHOLD_PX,
         onSlideBefore: function($slideElement, oldIndex, newIndex){
             //スライドする時に関数を呼び出す。newIndexはスライダーの現在地。
             slideChange(newIndex);
         }
     });
-    };
 
     /******************************************
-    スライドする時に発動する関数。タブの表示調整を行う。
-    *******************************************/
-    if(isScrolling == 0){   
+     スライドする時に発動する関数。タブの表示調整を行う。
+     *******************************************/
     function slideChange(newIndex){
 
         //クラスを調整
@@ -58,11 +83,10 @@ $(document).ready(function(){
         //スクロール位置を調整
         $('.tabContainer').animate({ scrollLeft: scrollDestination }, 'slow');
 
-    }
     };
     /******************************************
-    タブボタンクリックで発動する関数
-    *******************************************/
+     タブボタンクリックで発動する関数
+     *******************************************/
 
     $('.tab__button').on('click',function(e){
 
